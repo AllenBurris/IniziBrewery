c75182b109f93b6188ed5c29ec2223a0ea7bdf5a
AllenBurris <32074597+AllenBurris@users.noreply.github.com>
2018-09-13 13:07:41 -0400

    Plugin Startup/Shutdown
    Created functions for onEnable and onDisable

--- IniziBrew/src/net/inizicraft/inizibrew/IniziBrew.java
+++ IniziBrew/src/net/inizicraft/inizibrew/IniziBrew.java
@@ -1,14 +1,15 @@
 package net.inizicraft.inizibrew;
 
 import org.bukkit.Bukkit;
-import org.bukkit.inventory.Recipe;
 
 public class IniziBrew {
 
-	public static void main(String[] args) {
-		
-		Recipe brew1 = new Recipe();
-		Bukkit.addRecipe(brew1);
+	void onEnabled() {
+		Bukkit.getConsoleSender().sendMessage("IniziBrew Enabled");
+	}
+	
+	void onDisable() {
+		Bukkit.getConsoleSender().sendMessage("IniziBrew Disabled");
 	}
 	
 }
