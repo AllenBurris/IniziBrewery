3f39b52915fd59f8a56dfbaaae1df94f2fdc9ad6
AllenBurris <32074597+AllenBurris@users.noreply.github.com>
2018-09-13 10:53:04 -0400

    Started work

---
+++ IniziBrew/src/net/inizicraft/inizibrew/IniziBrew.java
@@ -0,0 +1,14 @@
+package net.inizicraft.inizibrew;
+
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Recipe;
+
+public class IniziBrew {
+
+	public static void main(String[] args) {
+		
+		Recipe brew1 = new Recipe();
+		Bukkit.addRecipe(brew1);
+	}
+	
+}
--- IniziBrew/src/net/inizicraft/inizibrew/package-info.java
+++ IniziBrew/src/net/inizicraft/inizibrew/package-info.java
@@ -4,5 +4,30 @@
 /**
  * @author StarBound6494
  *
- */
+ *
+#####################################################################
+#                         Custom Brewery 1.13                       #
+#                List of Ideas for mechanics and features           #
+#                             Version 0.1                           #
+#####################################################################
+
+
+Mechanics:
+- Follows most of the current brewery mechanics
+- multiblock structures no longer require signs, because that was kind of dumb
+- signs can be used to set tick amount on timer (see explanation 1)
+
+MultiBlocks:
+- distillery
+- barrel
+- large barrel
+- advanced distillery
+- fermenting machine of sorts (will improve this in a later version of the notes)
+- timer (for automation, explanation 1)
+
+Explanations:
+1. This allows us to require tick perfection on the recipes making them much more difficult to get perfect(nacre pearl anyone?) making recipes much more
+difficult to achieve perfection. This rewards players for successful automation but also makes automating require lots of tweaking to get just right.
+*/
+
 package net.inizicraft.inizibrew;
